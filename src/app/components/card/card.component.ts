0d2f3be0297a7df9cd2346a014a5fe33b3ea3a2b
la1ni <marilainarasilva@hotmail.com>
2024-10-04 16:04:25 -0300

    feat: criação da home

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+
+  @Input()
+  gameCover:string = ""
+
+  @Input()
+  gameLabel:string=""
+
+  @Input()
+  gameType:string=""
+  
+  @Input()
+  gamePrice:string=""
+
+}
